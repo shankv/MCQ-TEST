e5a075c9bbbbaf64faf926e23373d3b50b9b79bc
shashank verma <31140973+shankv@users.noreply.github.com>
2018-01-16 12:35:18 +0530

    Add files via upload

---
+++ Database/login.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: SELECT * FROM shashank.login
+LIMIT 0, 1000
+
+-- Date: 2017-08-09 13:34
+*/
+INSERT INTO `login` (`username`,`password`) VALUES ('a','a');
+INSERT INTO `login` (`username`,`password`) VALUES ('ankita','abc');
+INSERT INTO `login` (`username`,`password`) VALUES ('brajendra ','8622');
+INSERT INTO `login` (`username`,`password`) VALUES ('rekha','qwe');
+INSERT INTO `login` (`username`,`password`) VALUES ('shashank','abc');
